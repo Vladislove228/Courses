6bb66d36a959dcf330546150bea8c61c69039d19
Vladislove228 <wlad.151@mail.ru>
2023-05-12 11:12:33 +0200

    second commit

--- Courses/ViewController.swift
+++ Courses/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "dice-six-faces-three")
+        diceImageView2.image = #imageLiteral(resourceName: "dice-six-faces-three")
+    }
+    
 }
 
